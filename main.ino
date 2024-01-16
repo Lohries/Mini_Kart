40099d544dd88365d1ee45370e94c02c5092b05b
Luis Henrique <129761489+Lohries@users.noreply.github.com>
2024-01-15 21:51:59 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,4 +1,3 @@
-
 void setup() {
   pinMode(2, OUTPUT);
   pinMode(8, INPUT);
@@ -6,6 +5,13 @@ void setup() {
   pinMode(12, INPUT);
   pinMode(7, OUTPUT);
   pinMode(13, INPUT);
+
+  // Pinos ligados em IN3 e IN4,
+  pinMode(3, OUTPUT); // Configurando Pino 3 como saída de dados
+  pinMode(4, OUTPUT); // Configurando Pino 4 como saída de dados
+  pinMode(5, OUTPUT); // Configurando Pino 5 como saída de dados
+  pinMode(6, OUTPUT); // Configurando Pino 6 como saída de dados
+
   Serial.begin(9600);
 }
 
@@ -14,57 +20,48 @@ void loop() {
   float distancia2 = calculateDistance(4, 12);
   float distancia3 = calculateDistance(7, 13);
 
- 
   if (distancia1 <= 50) {
-    re();
-    direita();//Direita
-    
-    
+    re();  //Funciona
+    //direita(); 
   }
 
   if (distancia2 <= 50) {
-    re();//Ré
-    //Direita
-    
-  }
-  if (distancia3 <= 50) {
-    re();//Ré
-    esquerda()//Esquerda
-    
+    re(); // Funciona
+    // Direita
   }
 
-  
+  if (distancia3 <= 50) {
+    re(); // Ré
+    esquerda(); // Esquerda
+  }
+  frente();
+
+
+
+  // Repeat the above motor control logic as needed based on your application
+
   delay(100);
 }
 
-
-
 float calculateDistance(int triggerPin, int echoPin) {
   digitalWrite(triggerPin, LOW);
-  delay(2);
+  delayMicroseconds(2);
   digitalWrite(triggerPin, HIGH);
-  delay(10);
+  delayMicroseconds(10);
   digitalWrite(triggerPin, LOW);
-  float Dist = pulseIn(echoPin, HIGH) / 58.2;;
+  float Dist = pulseIn(echoPin, HIGH) / 58.2;
   return Dist;
 }
 
-void freio() {
-  digitalWrite(5, HIGH);  //Direita
-  digitalWrite(3, LOW);
-  
-  digitalWrite(9, HIGH); //Esquerda
-  digitalWrite(10, LOW);
-  delay(1500);
 
-}
+
 void frente() {
   digitalWrite(5, HIGH);
-  digitalWrite(3, HIGH);
+  digitalWrite(3, LOW);
   digitalWrite(9, HIGH);
-  digitalWrite(10, HIGH);
+  digitalWrite(10, LOW);
+  Serial.println("Frente");
   delay(1500);
-
 }
 
 void re() {
@@ -72,23 +69,22 @@ void re() {
   digitalWrite(3, HIGH);
   digitalWrite(9, LOW);
   digitalWrite(10, HIGH);
+  Serial.println("Ré");
   delay(1500);
-
-  
- }
-void direita () {
-  digitalWrite(9, HIGH); //Esquerda
-  digitalWrite(10, LOW);  
-
-  digitalWrite(5, LOW);  //Direita
-  digitalWrite(3, HIGH);
-
 }
-void esquerda () {
-  digitalWrite(9, LOW); //Esquerda
-  digitalWrite(10, HIGH);  
 
-  digitalWrite(5, HIGH);  //Direita
+void direita() {
+  digitalWrite(9, HIGH); // Esquerda
+  digitalWrite(10, LOW);
+  digitalWrite(5, LOW);  // Direita
+  digitalWrite(3, HIGH);
+  Serial.println("Direita");
+  delay(1000);
+}
+
+void esquerda() {
+  digitalWrite(9, LOW); // Esquerda
+  digitalWrite(10, HIGH);
+  digitalWrite(5, HIGH);  // Direita
   digitalWrite(3, LOW);
-  
 }
